a093d9c61b68d4ca32b0b8cd5b5cba7b7555b80f
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-04-19 04:02:05 +0200

    ini

--- src/main/java/login/loginController.java
+++ src/main/java/login/loginController.java
@@ -2,47 +2,47 @@ package login;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 
 public class loginController implements Initializable{
     @FXML
-    public TextField phone_text_sign_in;
+    private TextField phone_text_sign_in;
 
     @FXML
-    public TextField pass_text_sign_in;
+    private TextField pass_text_sign_in;
 
     @FXML
-    public ComboBox<String> type_sign_in;
+    private ComboBox<String> type_sign_in;
 
     @FXML
-    public Button sign_in_button;
+    private Button sign_in_button;
 
     @FXML
-    public TextField name_text_sign_up;
+    private TextField name_text_sign_up;
 
     @FXML
-    public TextField phone_text_sign_up;
+    private TextField phone_text_sign_up;
 
     @FXML
-    public TextField address_text_sign_up;
+    private TextField address_text_sign_up;
 
     @FXML
-    public TextField pass_text_sign_up;
+    private TextField pass_text_sign_up;
 
     @FXML
-    public ComboBox<String> type_sign_up;
+    private ComboBox type_sign_up;
 
     @FXML
-    public Button sign_up_button;
+    private Button sign_up_button;
 
     @FXML
     void sign_in_button_action(ActionEvent event) {
