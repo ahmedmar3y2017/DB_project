9891fb9ff20b830a0ec6851e4f3f1f031904b277
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-04-20 02:43:56 +0200

    ini

--- src/main/java/mongo/employee.java
+++ src/main/java/mongo/employee.java
@@ -1,5 +1,28 @@
 package mongo;
 
-public class employee {
 
-}
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+
+import java.util.List;
+
+public class employee {
+    static DBCollection dbCollection = mongoConnection.getDatabase().getCollection("employees");
+
+    // insert into database
+    public static BasicDBObject insertemployee(BasicDBObject basicDBObject) {
+
+        dbCollection.insert(basicDBObject);
+        return basicDBObject;
+    }
+
+
+    // select  employees
+    public static List<DBObject> selectemployees() {
+        DBCursor dbObjects = dbCollection.find();
+        return dbObjects.toArray();
+    }
+    }
+
